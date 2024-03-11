990ac765ce093d6cc7a6a7cfccff04305e6eb80a
mrki123@yahoo.comž <mrki123@yahoo.com>
2024-03-11 19:35:41 +0100

    started webshop

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { MainComponent } from "./components/MainComponent/MainComponent";
+import { FooterComponent } from "./components/FooterComponent/FooterComponent";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainComponent />
+      <FooterComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MainComponent/MainComponent.jsx
@@ -0,0 +1,9 @@
+function MainComponent() {
+  return (
+    <div>
+      <h1>Main</h1>
+    </div>
+  );
+}
+
+export { MainComponent };
